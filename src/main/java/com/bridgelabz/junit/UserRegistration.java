75e4fe6778651d8a4f193c05195d10b8d873eb9c
Janani <vsvprema@gmail.com>
2021-11-17 14:07:13 +0530

    Added: Test the Last name is correct

--- src/main/java/com/bridgelabz/junit/UserRegistration.java
+++ src/main/java/com/bridgelabz/junit/UserRegistration.java
@@ -15,4 +15,13 @@ public class UserRegistration {
             System.out.println("Please Enter a Valid First name\n FirstName should have Start with only one Capital\nFirstname should have atleast 3 characters");
         return check;
     }
+
+    public static boolean checkLastNameIsCorrect(String lastName) {
+        boolean check = Pattern.matches("^[A-Z]{1}+[a-z]{3,}+$", lastName);
+        if (check)
+            System.out.println("lastName is correct");
+        else
+            System.out.println("Please Enter a Valid last name\n lastName should have Start with only one Capital\nlastname should have atleast 3 characters");
+        return check;
+    }
 }
